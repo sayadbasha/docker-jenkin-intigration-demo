dd617c69828b16aac4e40263eaffaa7c0c1db53d
Basha <basha628@gmail.com>
2021-08-24 17:59:07 +0800

    first commit

---
+++ src/main/java/com/spring/springbootjenkindemo/DockerJenkinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springbootjenkindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/springbootjenkindemo/DockerJenkinDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootjenkindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
